b97079c0a74288e04295e300079fd2fb06c8ca29
Vince Trost <vincentjosephtrost@gmail.com>
2017-04-23 16:29:15 -0400

    update
    error fixes

--- Project/src/main/scala/Driver.scala
+++ Project/src/main/scala/Driver.scala
@@ -53,11 +53,11 @@ object Driver {
           val iterationCount = 100
           val clusterCount = 200
           val start = System.nanoTime
-          //cache data
           val model = KMeans.train(parsedData, clusterCount, iterationCount)
             //[id, lat, lon]
           val end = System.nanoTime
           println("KMeans Run-Time: " + (end - start) / 10e9 + "s")
+          
           val clusterCenters = model.clusterCenters map (_.toArray)
           val cost = model.computeCost(parsedData)
           println("Cost: " + cost)
@@ -70,11 +70,10 @@ object Driver {
 
           //show cluster centers and the number of members in them
           //write to file
-          val clusterInd = model.predict(data)
+          val clusterInd = model.predict(parsedData)
           clusterInd.collect()
           val clusterSizes = clusterInd.countByValue()
           val writer2 = new PrintWriter(new File("uberNightClusterSizes.txt"))
-          clusterSizes.collect()
           clusterSizes.foreach(x => writer2.write(x + "\n"))
           writer2.close()
 
